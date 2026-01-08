96ae8b3a9d82452f02a66a2693268dc6402bd200
Mika Kaakinen <mika.kaakinen@gmail.com>
2026-01-08 16:43:20 +0200

    10th commit

--- frontend/src/components/ProductCarousel.jsx
+++ frontend/src/components/ProductCarousel.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Carousel, Image } from 'react-bootstrap';
-import Loader from './Loader';
 import Message from './Message';
 import { useGetTopProductsQuery } from '../slices/productsApiSlice';
 
